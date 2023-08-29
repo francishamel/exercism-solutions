8cada89657419fe1f42173d503c59c2ecc716e2f
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2023-08-28 23:23:36 -0400

    Setup gleam env

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        devenv.shells.default = {
+          name = "exercism-solutions";
+
+          languages.gleam.enable = true;
+
+          packages = with pkgs; [
+            nil
+            nixpkgs-fmt
+          ];
+        };
+      };
+    };
+}
